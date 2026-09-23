6d9438f899ea15e138a9d041e4f4757cb36ad91c
RomanG_BPA2301 <gorulenko2005@mail.ru>
2026-09-23 23:38:43 +0700

    Выравнивание по краю области содержимого Column

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -51,8 +51,8 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.size(width = 400.dp, height = 200.dp)) {
+    Column(horizontalAlignment = Alignment.End,
+        modifier = modifier.width(250.dp)) {
         TextCell("1")
         TextCell("2")
         TextCell("3")
