f4e6ec42494da31de183e194cc75f33c92abed46
RomanG_BPA2301 <gorulenko2005@mail.ru>
2026-09-23 23:53:00 +0700

    Дочерние элементы располагаются на равном расстоянии друг от друга, включая пространство перед первым и после последнего

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -51,8 +51,8 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(verticalArrangement = Arrangement.Bottom,
-        modifier = modifier.height(400.dp)) {
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier.width(1000.dp)) {
         TextCell("1")
         TextCell("2")
         TextCell("3")
