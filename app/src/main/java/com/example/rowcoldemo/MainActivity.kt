28837370c91938c8896eeaa067928120d7257fcf
RomanG_BPA2301 <gorulenko2005@mail.ru>
2026-09-24 00:26:54 +0700

    К small text добавляется дополнительное вертикальное смещение в 80 dp

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.FirstBaseline
 import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,15 +56,16 @@ fun MainScreen(modifier: Modifier = Modifier) {
     Row {
         Text(
             text = "Large Text\n\nMore Text",
-            Modifier.alignBy(LastBaseline),
+            Modifier.alignBy(FirstBaseline),
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold
         )
         Text(
             text = "Small Text",
-            Modifier.alignByBaseline(),
+            modifier = Modifier.paddingFrom(
+                alignmentLine = FirstBaseline, before = 80.dp, after = 0.dp),
             fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
+            fontWeight = FontWeight.Bold
         )
     }
 }
